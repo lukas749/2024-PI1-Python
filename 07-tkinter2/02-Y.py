ef91c1ec3ba83892ff1ac94c8b4eeb7a17ee4052
Lukáš Mäkký <lukas896698@gmail.com>
2024-11-13 10:08:23 +0100

    add.07-tkinter2

---
+++ 07-tkinter2/02-Y.py
@@ -0,0 +1,35 @@
+import tkinter
+
+canvas = tkinter.Canvas(width=600, height=600)
+canvas.pack()
+
+def xko(x,y):
+    canvas.create_rectangle(x,y,x+10,y+10,fill="yellow")
+    canvas.create_rectangle(x,y+10,x+10,y+20,fill="yellow")
+    canvas.create_rectangle(x+10,y+20,x+20,y+30,fill="yellow")
+    canvas.create_rectangle(x+30,y+20,x+40,y+30,fill="yellow")
+    canvas.create_rectangle(x+40,y,x+50,y+10,fill="yellow")
+    canvas.create_rectangle(x+40,y+10,x+50,y+20,fill="yellow")
+    canvas.create_rectangle(x+20,y+30,x+30,y+40,fill="yellow")
+    canvas.create_rectangle(x+20,y+40,x+30,y+50,fill="yellow")
+    canvas.create_rectangle(x+20,y+50,x+30,y+60,fill="yellow")
+    canvas.create_rectangle(x+20,y+60,x+30,y+70,fill="yellow")
+x=10
+y=10
+def riadok_x(x,y,pocet):
+    for i in range (pocet):
+        xko(x,y)
+        x +=60
+
+def riadky_x(x,y,pocet_riadkov,pocet_stlpcov):
+    for i in range(pocet_riadkov):
+        riadok_x(x,y,pocet_stlpcov)
+        y +=80
+
+xko(10,10)
+riadok_x(10,100,3)
+riadky_x(10,180,3,5)
+
+
+
+canvas.mainloop()
