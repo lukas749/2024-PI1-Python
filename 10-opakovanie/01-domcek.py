11c1510b9d8539b301c39e0dced26d08cfec03ff
Lukáš Mäkký <lukas896698@gmail.com>
2024-12-11 10:00:19 +0100

    add.10-opakovanie

--- 10-opakovanie/01-domcek.py
+++ 10-opakovanie/01-domcek.py
@@ -3,8 +3,8 @@ import tkinter
 x = 100
 y = 100
 dlzka = 100
-farba = "yellow"
 
+pocet = 5
 canvas_height = int(input("Zadaj výšku plátna:"))
 canvas_width = int(input("Zadaj šírku plátna:"))
 
@@ -15,11 +15,11 @@ canvas.pack()
 def dom(x, y, dlzka, farba):
     stvrtina = dlzka / 4
     #canvas.create_polygon(x0,y0,x1,y1,x2,y2) # vykresli mnohouholník v tomto prípade trojuholník, lebo prejde cez 3 body (x,y)
-    canvas.create_polygon(x, y + dlzka / 2, x + dlzka / 2, y, x + dlzka, y + dlzka / 2, fill = "red")
-    canvas.create_rectangle(x, y + dlzka / 2, x + dlzka, y + dlzka / 2 + dlzka,fill = farba)
-    canvas.create_rectangle(x + stvrtina, y + dlzka/2 + stvrtina, x + stvrtina * 3, y + dlzka/2 + stvrtina * 3, fill = "light blue")
-    canvas.create_line(x + stvrtina, y + dlzka, x + stvrtina * 3, y + dlzka)
-    canvas.create_line(x + dlzka / 2, y + dlzka / 2 + stvrtina, x + dlzka / 2, y + dlzka + dlzka/2 - stvrtina)
+    canvas.create_polygon(x/2, y/3 + dlzka / 2, x/2 + dlzka / 2, y/3, x/2 + dlzka, y/3 + dlzka / 2, fill = "red")
+    canvas.create_rectangle(x/2, y/3 + dlzka / 2, x/2 + dlzka, y/3 + dlzka / 2 + dlzka,fill = farba)
+    canvas.create_rectangle(x/2 + stvrtina, y/3 + dlzka/2 + stvrtina, x/2 + stvrtina * 3, y/3 + dlzka/2 + stvrtina * 3, fill = "light blue")
+    canvas.create_line(x/2 + stvrtina, y/3 + dlzka, x/2 + stvrtina * 3, y/3 + dlzka)
+    canvas.create_line(x/2 + dlzka / 2, y/3 + dlzka / 2 + stvrtina, x/2 + dlzka / 2, y/3 + dlzka + dlzka/2 - stvrtina)
     
     
     
@@ -30,5 +30,20 @@ def dom(x, y, dlzka, farba):
     #canvas.create_rectangle(x + stvrtina, y + dlzka/2 + stvrtina, x + dlzka/2, y + dlzka)
     #canvas.create_rectangle(x + stvrtina, y + dlzka + stvrtina, x + dlzka/2, y + dlzka  )
     #canvas.create_rectangle(x + dlzka/2, y + dlzka/2 + stvrtina, x + stvrtina * 3, y + dlzka) pomocou stvorcou urobene okno
-dom(x, y, dlzka, farba)
+
+def ulica(x,y,dlzka,pocet):
+    farba1, farba2 = "red","blue"
+    for i in range (5):
+        dom(x,y,dlzka,farba1)
+        x = x + dlzka + 120
+        farba1, farba2 = farba2, farba1
+
+def stlpec(x,y,dlzka,pocet):
+    farba1, farba2 = "red","blue"
+    for i in range(5):
+        ulica(x,y,dlzka,farba1)
+        y = y + dlzka + 380
+
+stlpec(0,0,dlzka,5)
+ulica(0,0,dlzka,5)
 canvas.mainloop()
--- 10-opakovanie/02-ulica.py
+++ 10-opakovanie/02-ulica.py
@@ -4,8 +4,7 @@ x = 50
 y = 50
 dlzka = 100
 pocet = 10
-farba1 = "blue"
-farba2 ="red"
+
 
 canvas_height = int(input("Zadaj výšku plátna:"))
 canvas_width = int(input("Zadaj šírku plátna:"))
@@ -15,6 +14,8 @@ canvas.pack()
 
 
 def ulica (x, y, dlzka, pocet):
+    farba1, farba2 = "red", "blue"
+    
     stvrtina = dlzka / 4
     
     canvas.create_polygon(x , y + dlzka / 2, x + dlzka / 2 , y, x + dlzka , y + dlzka / 2, fill = "red")
@@ -22,8 +23,6 @@ def ulica (x, y, dlzka, pocet):
     canvas.create_rectangle(x + stvrtina, y + dlzka/2 + stvrtina, x + stvrtina * 3, y + dlzka/2 + stvrtina * 3, fill = "light blue")
     canvas.create_line(x + stvrtina, y + dlzka, x + stvrtina * 3, y + dlzka)
     canvas.create_line(x + dlzka / 2 , y + dlzka / 2 + stvrtina, x + dlzka / 2, y + dlzka + dlzka/2 - stvrtina)
-    
-    
     
     canvas.create_polygon(x + 150, y + dlzka / 2, x + dlzka / 2 + 150, y, x + dlzka + 150, y + dlzka / 2, fill = "red")
     canvas.create_rectangle(x + 150, y + dlzka / 2, x + dlzka + 150, y + dlzka / 2 + dlzka,fill = farba1)
@@ -37,16 +36,14 @@ def ulica (x, y, dlzka, pocet):
     canvas.create_line(x + stvrtina + 300, y + dlzka, x + stvrtina * 3 + 300, y + dlzka)
     canvas.create_line(x + dlzka / 2 + 300, y + dlzka / 2 + stvrtina, x + dlzka / 2 + 300, y + dlzka + dlzka/2 - stvrtina)
     
+    farba1, farba2 = farba2, farba1
 
 
 def rad(x,y,dlzka,pocet):
     for i in range(pocet):
         ulica(x,y,dlzka,pocet)
         x += 150
-    if  x < 3 * x:
-        farba= farba1
-    else:
-        farba= farba2
+
 
 def stlpec(x,y,dlzka,pocet):
     for i in range(pocet):
