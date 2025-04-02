6c808bc7078346c2cfa259b871c483e569417702
Lukáš Mäkký <lukas896698@gmail.com>
2025-04-02 10:04:50 +0200

    add 17-list

--- 16-string2/04-du.py
+++ 16-string2/04-du.py
@@ -1,2 +1,5 @@
 # uživateľ zadá celú vetu a program  vetu rozdelí na slová, ktoré vypíše náhodnou farbou na náhodnom mieste
 # aj písmenká zo slov vypíše náhodnými farbami
+
+s = '12345'
+s.isdigit()
